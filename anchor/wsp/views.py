28940f0aa5db55dbb7bc2bf0783a1063d32f5748
Tife28 <boludayo@ekunstech.com>
2022-07-16 12:22:31 +0100

    Modified

--- anchor/wsp/views.py
+++ anchor/wsp/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render, redirect  
 from django.http import HttpResponse
-from .forms import StudentForm  
-from .models import Student  
+from .forms import *
+from .models import *  
 
 # Create your views here.  
 def home(request):  
     if request.method == "POST":  
-        form = StudentForm(request.POST)  
+        form = StudentForm(request.POST)
+        print(dict(form.errors))
         if form.is_valid():  
             try:  
                 form.save()  
                 return HttpResponse("<h3>Application successfully submitted!</h3><br><center><a href='' class='btn btn-primary'>Apply to Work Study Programme</a></center>")
-                #return redirect('/show')  
             except:  
                 pass  
     else:  
@@ -25,11 +25,26 @@ def edit(request, id):
     return render(request,'edit.html', {'student':student})  
 def update(request, id):  
     student = Student.objects.get(id=id)  
-    form = StudentForm(request.POST, instance = student)  
+    form = StudentForm(request.POST, instance = student) 
+    # form1 = RecommendationForm(request.POST, instance = form) 
     if form.is_valid():  
         form.save()  
         return redirect("/show")  
-    return render(request, 'edit.html', {'student': student})  
+
+    # if request.method == "POST":  
+    #     form1 = RecommendationForm(request.POST)
+    #     print(dict(form.errors))
+    #     if form.is_valid():  
+    #         if form1.is_valid():
+    #             try:  
+    #                 form.save()
+    #                 form1.save()  
+    #                 return HttpResponse("<h3>Application successfully submitted!</h3><br><center><a href='' class='btn btn-primary'>Apply to Work Study Programme</a></center>")
+    #             except:  
+    #                 pass  
+    # else:  
+    #     form = RecommendationForm()  
+    return render(request, 'edit.html', {'student': student}) #'form1': form1 
 def destroy(request, id):  
     student = Student.objects.get(id=id)  
     student.delete()  
